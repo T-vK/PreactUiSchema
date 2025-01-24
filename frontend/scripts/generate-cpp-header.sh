59556442013dd5d339056a7101b317618d3e4ac2
T-vK <T-vK@users.noreply.github.com>
2025-01-24 18:13:40 +0100

    feat: Use const types to store html in flash instead of ram

--- frontend/scripts/generate-cpp-header.sh
+++ frontend/scripts/generate-cpp-header.sh
@@ -6,16 +6,20 @@ HEADER_FILE="$1"
 rm -f "$HEADER_FILE"
 
 # Read the binary content of the file
-C_ARRAY_CODE=$(xxd -i dist/index.html.br)
+C_ARRAY_CODE=$(xxd -i dist/index.html.br | sed 's/unsigned char/const uint8_t/' | sed 's/unsigned int/const uint32_t/')
 
 # Create the header file and write the content
 cat <<EOL > $HEADER_FILE
 #ifndef GENERATED_HTML_H
 #define GENERATED_HTML_H
 
+#ifdef ARDUINO
+#include <Arduino.h>
+#else
 #include <cstdint>
+#endif
 
-// Binary content of index.html.br as char array
+// Binary content of index.html.br as const uint8_t array
 $C_ARRAY_CODE
 
 #endif // GENERATED_HTML_H
